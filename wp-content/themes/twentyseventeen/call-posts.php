bec1bf48bc49a9e902c1d4ce9b1591aaf65acf2e
Yasir Sarole <yasirs@prdxn.com>
2018-04-20 10:41:23 +0530

    added code for calling post function in call-posts.php

---
+++ wp-content/themes/twentyseventeen/call-posts.php
@@ -0,0 +1,9 @@
+<?php include 'page-custompostpage.php';?>
+
+<?php
+$PostType = 'post';
+$PostStatus = 'publish';
+$PostsPerPage = -1;
+CustomPostCall($PostType, $PostStatus, $PostsPerPage);
+?>
+
--- wp-content/themes/twentyseventeen/page-custompostpage.php
+++ wp-content/themes/twentyseventeen/page-custompostpage.php
@@ -1,37 +1,30 @@
-	<?php
-	$PostType = 'post';
-	$PostStatus = 'publish';
-	$PostsPerPage = -1;
-	CustomPostCall($PostType, $PostStatus, $PostsPerPage);
+<?php
+function CustomPostCall($PostType, $PostStatus, $PostsPerPage) {
+
+	// In the below array we can set more parameters and pass along to the function
+	$args = array (
+		'post_type' => $PostType,
+		'post_status' => $PostStatus,
+		'posts_per_page' => $PostsPerPage,
+	);
+
+	$the_query = new WP_Query($args); //calling the post with the above arguments
 	?>
 
-	<?php
-	function CustomPostCall($PostType, $PostStatus, $PostsPerPage) {
+	<?php if ( $the_query->have_posts() ) : ?>
 
-		// In the below array we can set more parameters and pass along to the function
-		$args = array (
-			'post_type' => $PostType,
-			'post_status' => $PostStatus,
-			'posts_per_page' => $PostsPerPage,
-		);
+	<ul>
+	<!-- the loop -->
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+	    <li>
+	    	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	    	<?php the_content(); ?>
+	    </li>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
+	</ul>
 
-		$the_query = new WP_Query($args); //calling the post with the above arguments
-		?>
+	<?php wp_reset_postdata(); ?>
 
-		<?php if ( $the_query->have_posts() ) : ?>
-
-		<ul>
-		<!-- the loop -->
-		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-		    <li>
-		    	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		    	<?php the_content(); ?>
-		    </li>
-		<?php endwhile; ?>
-		<!-- end of the loop -->
-		</ul>
-
-		<?php wp_reset_postdata(); ?>
-
-		<?php endif; ?>
-	<?php } ?>
+	<?php endif; ?>
+<?php } ?>
